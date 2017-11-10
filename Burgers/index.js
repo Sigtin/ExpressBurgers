7b1332d0976c2a95c6f2526e702f88d187b00d9a
Unknown <asigler@student.neumont.edu>
2017-11-10 13:36:25 -0700

    Pictures now on the drinks page

--- Burgers/index.js
+++ Burgers/index.js
@@ -3,7 +3,7 @@ var pug = require('pug');
 var path = require('path');
 var config = require('./config.json');
 var entrees = require('./entrees.json');
-// var drinks = require('./drinks.json');
+var drinks = require('./drinks.json');
 // var sides = require('./sides.json');
 // var desserts = require('./desserts.json');
 
@@ -22,7 +22,8 @@ app.get('/', function(req, res){
 app.get('/:viewname', function(req, res){
     res.render(req.params.viewname, {
         "config": config,
-        "entrees": entrees
+        "entrees": entrees,
+        "drinks": drinks
     });
 });
 
